a542c04516204c17402f0ef49ab70cf8200c6506
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-04-29 19:52:49 -0700

    added Scoped service class

---
+++ COMP003B.Assignment6/Services/MyScopedService.cs
@@ -0,0 +1,21 @@
+﻿namespace COMP003B.Assignment6.Services
+{
+    public class MyScopedService
+    {
+        // private field
+        private readonly Guid _uniqueId;
+
+        // default constructor
+        public MyScopedService()
+        {
+            _uniqueId = Guid.NewGuid();
+        }
+
+        // public method that returns _uniqueId value
+        public Guid GetUniqueId()
+        {
+            return _uniqueId;
+
+        }
+    }
+}
