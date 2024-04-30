df0d673de029aaa23040f28e743b5a35b5157264
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-04-29 19:50:47 -0700

    Add project files.

---
+++ COMP003B.Assignment6/Services/MyTransientService.cs
@@ -0,0 +1,20 @@
+﻿namespace COMP003B.Assignment6.Services
+{
+    public class MyTransientService
+    {
+        // private field
+        private readonly Guid _uniqueId;
+
+        // default constructor
+        public MyTransientService()
+        {
+            _uniqueId = Guid.NewGuid();
+        }
+
+        // public method
+        public Guid GetUniqueId()
+        {
+            return _uniqueId;
+        }
+    }
+}
