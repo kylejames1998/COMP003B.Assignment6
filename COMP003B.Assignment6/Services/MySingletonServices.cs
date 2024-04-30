1a4c2f8bdbc9996cca484f3e83ceac5a9a70ece5
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-04-29 19:54:26 -0700

    added Singleton service class

---
+++ COMP003B.Assignment6/Services/MySingletonServices.cs
@@ -0,0 +1,20 @@
+﻿namespace COMP003B.Assignment6.Services
+{
+    public class MySingletonServices
+    {
+        // private field
+        private readonly Guid _uniqueId;
+
+        // default constructor
+        public MySingletonServices()
+        {
+            _uniqueId = Guid.NewGuid();
+        }
+
+        // public method that returns the _uniqueId value
+        public Guid GetUniqueId()
+        {
+            return _uniqueId;
+        }
+    }
+}
